8b1b68967cb2d33cf6789c8cceb4973404741157
Muhammad Imam arif darmawan <muhamadimamarif225@gmail.com>
2025-01-08 09:35:11 +0700

    feat:migration

---
+++ app/Http/Controllers/CompantAboutController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CompantAbout;
+use Illuminate\Http\Request;
+
+class CompantAboutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CompantAbout $compantAbout)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CompantAbout $compantAbout)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CompantAbout $compantAbout)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CompantAbout $compantAbout)
+    {
+        //
+    }
+}
